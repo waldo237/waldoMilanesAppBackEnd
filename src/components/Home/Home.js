fbf8ce96dc68ce03fe14e0b8095bbee48a244960
waldo237 <waldo23792@gmail.com>
2020-05-13 00:25:05 -0400

    some changes to UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Home.css'
-import waldoImg from '../../static/waldo.jpg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
-import { useTransition, animated } from 'react-spring'
+import { useTransition, useSpring, animated } from 'react-spring'
 
 const MainCard = () => {
+    const [toTheRight, moveMore] = useState(0);
+    useEffect(()=>{
+        window.onscroll = ()=> {
+        //     moveMore({toTheRight: window.pageYOffset});
+        const img =  document.querySelector('.img');
+        img.style.setProperty('transform',  `translate3d(${window.pageYOffset},${window.pageYOffset},0)`)
+        console.log(window.pageYOffset);
+    }
+});
+    
+    
     return <>
+
         <div className='img-wrapper'>
-            <img src={waldoImg} alt='Waldo Milanes' className='mw-img' />
-            <div >
-                <h4 className='name'>Waldo Milanes</h4>
-                <small className='job-title'>Full-stack developer</small>
-            </div>
-            <div className='icon-container'>
-                <FontAwesomeIcon className='fa-lg primary--text chevron-down' icon={faAngleDoubleDown} />
-            </div>
+            <div className='img'></div>
+            <h4 className='name'>Waldo Milanes</h4>
+            <small className='job-title'>Full-stack developer</small>
         </div>
     </>
 }
@@ -23,15 +27,15 @@ const MainCard = () => {
 const Home = () => {
     const [open, toggle] = useState(true);
     const transitions = useTransition(open, null, {
-        from: { opacity:0, transform: 'scaleY(0)' },
-        enter: { opacity:1, transform: 'scaleY(1)' },
-        leave: { opacity:0, transform: 'scaleY(1)' },
+        from: { opacity: 0, transform: 'scaleY(0)' },
+        enter: { opacity: 1, transform: 'scaleY(1)' },
+        leave: { opacity: 0, transform: 'scaleY(1)' },
     })
     const transition2 = useTransition(open, null, {
-        from: { opacity:0, transform: 'translate3d(0,500px,0)' },
-        enter: { opacity:1, transform: 'translate3d(0,0,0)' },
-        leave: { opacity:0, transform: 'translate3d(0,500px,0)' },
-        
+        from: { opacity: 0, transform: 'translate3d(0,500px,0)' },
+        enter: { opacity: 1, transform: 'translate3d(0,0,0)' },
+        leave: { opacity: 0, transform: 'translate3d(0,500px,0)' },
+
     })
 
     return (
@@ -42,8 +46,6 @@ const Home = () => {
                         item
                             ? <animated.div style={props} key={key}><MainCard /></animated.div>
                             : <animated.div style={props} key={key}>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
                             </animated.div>)}
                 </div>
@@ -58,12 +60,12 @@ const Home = () => {
                 )}
             </article>
             <aside className='secondary'>
-               
+
                 {transition2.map(({ item, key, props }) =>
                     <animated.div style={props} key={key}>
                         <h1>education</h1>
-                <p>
-                    Lorem Ipsum is dummy.
+                        <p>
+                            Lorem Ipsum is dummy.
                 </p>
                     </animated.div>
                 )}
