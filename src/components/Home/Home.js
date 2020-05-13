260bdb1031414dacc9e1bc67fc76c26761f976b2
waldo237 <waldo23792@gmail.com>
2020-05-13 19:33:54 -0400

    some changes to UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,20 +4,29 @@ import { useTransition, useSpring, animated } from 'react-spring'
 
 const MainCard = () => {
     const [toTheRight, moveMore] = useState(0);
-    useEffect(()=>{
-        window.onscroll = ()=> {
-        //     moveMore({toTheRight: window.pageYOffset});
-        const img =  document.querySelector('.img');
-        img.style.setProperty('transform',  `translate3d(${window.pageYOffset},${window.pageYOffset},0)`)
-        console.log(window.pageYOffset);
-    }
-});
-    
-    
+    useEffect(() => {
+        const wmImg = document.querySelector(".wm-img");
+        const wrapper = document.querySelector("#description");
+
+        const wrapperObserver = new IntersectionObserver( (entries, wrapperObserver) =>{
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    wmImg.classList.add("img-scrolled");
+                    wrapper.classList.add('descriptionIn');
+                } else {
+                    wmImg.classList.remove("img-scrolled");
+                }
+               
+            });
+        });
+        wrapperObserver.observe(wrapper);
+    });
+
+
     return <>
 
         <div className='img-wrapper'>
-            <div className='img'></div>
+            <div className='wm-img'></div>
             <h4 className='name'>Waldo Milanes</h4>
             <small className='job-title'>Full-stack developer</small>
         </div>
@@ -43,11 +52,10 @@ const Home = () => {
             <main className='main light'>
                 <div onClick={() => toggle(!open)}>
                     {transitions.map(({ item, key, props }) =>
-                        item
-                            ? <animated.div style={props} key={key}><MainCard /></animated.div>
-                            : <animated.div style={props} key={key}>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-                            </animated.div>)}
+                        <animated.div style={props} key={key}><MainCard /></animated.div>)}
+                </div>
+                <div id='description'>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
                 </div>
             </main>
             <article className='light'>
