703eaf94c659b9a1bc103f0e1513338c33ace972
waldo237 <waldo23792@gmail.com>
2020-07-26 17:05:00 -0400

    fish tank with my work

---
+++ src/components/Home/Technologies.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import "./Technologies.css";
+import { Link } from "react-router-dom";
+import {
+  faNodeJs,
+  faJava,
+  faVuejs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Technologies = () => {
+  useEffect(() => {
+    document.title = "Projects";
+  }, []);
+  const projects = [
+    {
+      url: "/project/node",
+      icon: faNodeJs,
+      title: "NodeJs",
+
+    },
+    {
+      url: "/project/java",
+      icon: faJava,
+      title: "Java",
+
+    },
+    {
+      url: "/project/vue",
+      icon: faVuejs,
+      title: "VueJS",
+
+    },
+    {
+      url: "/project/react",
+      icon: faReact,
+      title: "ReactJS",
+  
+    },
+  ];
+  return (
+    <>
+      <div className="technology-wrapper">
+        {projects.map((project) => (
+          <article
+            key={project.url}
+            className="hoverable-card"
+          >
+            <Link to={project.url} className="link">
+              <div className="technology-card">
+                <FontAwesomeIcon
+                  className="fa-5x primary--text"
+                  icon={project.icon}
+                />
+                <h1 className=" light--text ">
+                  {project.title}
+                </h1>
+              </div>
+            </Link>
+
+          </article>
+        ))}
+
+      </div>
+    </>
+  );
+};
+
+export default Technologies;
