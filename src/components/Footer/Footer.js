a270f2699e044d43af2533c289633a61fb8b91ab
waldo237 <waldo23792@gmail.com>
2020-07-27 00:51:25 -0400

    added new links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,14 +7,44 @@ import {
   faFacebookSquare,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 const Footer = () => (
   <footer className="primary footer">
     <div>
-      <FontAwesomeIcon className="fa-2x social" icon={faTwitter} />
-      <FontAwesomeIcon className="fa-2x social" icon={faLinkedin} />
-      <FontAwesomeIcon className="fa-2x social" icon={faFacebookSquare} />
-      <FontAwesomeIcon className="fa-2x social" icon={faInstagram} />
+      <a
+        rel="noopener noreferrer"
+        href='https://twitter.com/WaldoMilanes'
+        target="_blank"
+        className="link hoverable-card social-link"
+      >
+        <FontAwesomeIcon className="fa-2x social" icon={faTwitter} />
+
+      </a>
+      <a
+        rel="noopener noreferrer"
+        href='https://www.linkedin.com/in/waldomilanes/'
+        target="_blank"
+        className="link hoverable-card social-link"
+      >
+        <FontAwesomeIcon className="fa-2x social" icon={faLinkedin} />
+      </a>
+      <a
+        rel="noopener noreferrer"
+        href='https://www.facebook.com/milanes237/'
+        target="_blank"
+        className="link hoverable-card social-link"
+      >
+        <FontAwesomeIcon className="fa-2x social" icon={faFacebookSquare} />
+      </a>
+      <a
+        rel="noopener noreferrer"
+        href='https://www.instagram.com/waldo237/?hl=en'
+        target="_blank"
+        className="link hoverable-card social-link"
+      >
+        <FontAwesomeIcon className="fa-2x social" icon={faInstagram} />
+      </a>
     </div>
     <div className="footer-credits">
       {" "}
@@ -22,8 +52,11 @@ const Footer = () => (
         &#9400; 2019-
         {new Date().getFullYear()} &nbsp;{" "}
       </span>{" "}
-      <span> | W PROGRAMMING | </span> <span>&nbsp;waldomilanes.com </span>
+      <Link to="/" className="social-link link">
+        <span> | W PROGRAMMING | </span> <span>&nbsp;waldomilanes.com </span>
+      </Link>
     </div>
+
     <div>
       developed by Waldo Milanes
       <span>&reg;</span>
