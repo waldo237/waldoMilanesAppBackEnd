423c1c6c40cbe1cf2ca37764d2b39f0bba347c29
waldo237 <waldo23792@gmail.com>
2020-12-01 23:10:07 -0400

    Edited the graphql schema

--- src/models/projectGraphQLSchema.js
+++ src/models/projectGraphQLSchema.js
@@ -1,11 +1,20 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-type Dir {
-    _id: ID!
-    name:String!
-    type: String!
-    content: Dir
+type dir {
+    dir: [folder]
+}
+type FILE {
+   _id: ID
+   name:String
+   type: String
+   content: String
+}
+type folder {
+   _id: ID
+   name:String
+   type: String
+   content: [FILE]
 }
 
 
@@ -28,8 +37,7 @@ technology: String!
 description: String!
 url: String!
 screenshot: String!
-code: [Dir]
-iterate(_id: ID): Dir
+code: dir
 date: String!
 comments: [Comment]
 rating: [Rating]
@@ -44,6 +52,5 @@ project(_id: ID): Project
 
 schema {
 query: Query
-
 }
 `);
